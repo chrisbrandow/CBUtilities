1b07ba1b35ef1925c170b33a82b055abdd1c3700
chrisbrandow <chrisbrandow@gmail.com>
2023-01-28 14:58:50 -0800

    add dispatch with delay

---
+++ Sources/CBUtilities/DispatchQueueExtension.swift
@@ -0,0 +1,15 @@
+//
+//  DispatchQueueExtension.swift
+//  
+//
+//  Created by Chris Brandow on 1/28/23.
+//
+
+import Foundation
+
+extension DispatchQueue {
+    public func cb_perform(block: @escaping (() -> Void), withDelay delay: TimeInterval) {
+        let dispatchTime = DispatchTime.now() + delay
+        self.asyncAfter(deadline: dispatchTime, execute: block)
+    }
+}
